49d19c5a957d2c0bf931f653bd9ad894c654c0a6
aharrison <allie.harrison@gmail.com>
2016-09-29 16:10:57 -0500

    Added private method

--- src/com/arkansascodingacademy/example/HelloWorld.java
+++ src/com/arkansascodingacademy/example/HelloWorld.java
@@ -4,7 +4,13 @@ package com.arkansascodingacademy.example;
 public class HelloWorld {
 
     public static void main(String[] args) {
-
+        HelloWorld helloWorld = new HelloWorld();
+        String phrase = helloWorld.sayHello();
+        System.out.println(phrase);
+    }
+    
+    private String sayHello() {
+        return "Hello World!";
     }
     
 }
