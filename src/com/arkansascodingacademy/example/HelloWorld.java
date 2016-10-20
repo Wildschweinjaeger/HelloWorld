12b713cb3dce9002ddfe8ce6a0f21f65c45abfc9
allie_wtd25pq <allie_wtd25pq@DESKTOP-3THCLE4>
2016-10-20 10:53:20 -0500

    Added another private method

--- src/com/arkansascodingacademy/example/HelloWorld.java
+++ src/com/arkansascodingacademy/example/HelloWorld.java
@@ -7,10 +7,15 @@ public class HelloWorld {
         HelloWorld helloWorld = new HelloWorld();
         String phrase = helloWorld.sayHello();
         System.out.println(phrase);
+        phrase = helloWorld.harrisonSayHello();
+        System.out.println(phrase);
     }
     
     private String sayHello() {
         return "Hello World!";
     }
     
+    private String harrisonSayHello() {
+        return "Hello from Allie";
+    }
 }
